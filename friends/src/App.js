73d2e8b6ccd62285115fc6319afb47d5eda2fa21
Adnan <adnan.chowdhury@outlook.com>
2019-08-20 15:35:31 -0500

    App Home page and login form created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import "./App.css";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">Lets authenticate some stuff</div>;
+  return (
+    <div className="App">
+      <Route path="/" exact component={Home} />
+      <Route path="/login" component={Login} />
+    </div>
+  );
 }
 
 export default App;
